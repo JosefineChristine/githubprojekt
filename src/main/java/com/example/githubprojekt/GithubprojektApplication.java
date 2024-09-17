875f87943c891b482aadbdca30e8f270f70cec5c
JosefineChristine <joro0003@stud.kea.dk>
2024-09-17 13:32:07 +0200

    Initial commit

---
+++ src/main/java/com/example/githubprojekt/GithubprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubprojekt/GithubprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
